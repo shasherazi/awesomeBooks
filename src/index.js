c1d6dfe5be93c5d4ae3f4a52e16cf0241e156c59
shasherazi <hassanrandomz@gmail.com>
2023-01-10 15:02:19 +0500

    Refactor code with classes

--- src/index.js
+++ src/index.js
@@ -1,63 +1,70 @@
-class Books {
-  constructor() {
-    this.booksList = document.querySelector('.main-books-list-ul');
-    this.form = document.querySelector('.main-form-form');
-    this.formTitle = document.querySelector('.main-form-form-title');
-    this.formAuthor = document.querySelector('.main-form-form-author');
-    this.books = [];
+const booksList = document.querySelector('.main-books-list-ul');
+const form = document.querySelector('.main-form-form');
+const formTitle = document.querySelector('.main-form-form-title');
+const formAuthor = document.querySelector('.main-form-form-author');
 
-    this.form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      this.addBookToView(this.formTitle.value, this.formAuthor.value);
-      this.updateLocalStorage();
-    });
-
-    document.addEventListener('click', (e) => {
-      if (e.target.classList.contains('main-books-list-ul-li-button')) {
-        e.target.parentElement.remove();
-
-        const title = e.target.parentElement.querySelector('.main-books-list-ul-li-title').innerText;
-        this.removeBook(title);
-        this.updateLocalStorage();
-      }
-    });
-
-    document.addEventListener('DOMContentLoaded', () => {
-      const books = JSON.parse(localStorage.getItem('booksArray'));
-      if (books) {
-        books.forEach((book) => {
-          this.addBookToView(book.title, book.author);
-        });
-      }
-    });
-  }
-
-  updateLocalStorage() {
-    localStorage.setItem('booksArray', JSON.stringify(this.books));
-  }
-
-  removeBook(title) {
-    this.books = this.books.filter((book) => book.title !== title);
+let books = [];
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
 
   addBookToView(title, author) {
-    this.books.push({
+    books.push({
       title,
       author,
     });
-    this.formTitle.value = '';
-    this.formAuthor.value = '';
+
+    formTitle.value = '';
+    formAuthor.value = '';
 
     const bookItem = document.createElement('li');
     bookItem.classList.add('main-books-list-ul-li');
     bookItem.innerHTML = `
-      <p class="main-books-list-ul-li-title">${title}</p>
-      <p class="main-books-list-ul-li-author">${author}</p>
-      <button class="main-books-list-ul-li-button">Remove</button>
-      <hr class="main-books-list-ul-li-hr">
-    `;
-    this.booksList.appendChild(bookItem);
+            <p class="main-books-list-ul-li-title">${title}</p>
+            <p class="main-books-list-ul-li-author">${author}</p>
+            <button class="main-books-list-ul-li-button">Remove</button>
+            <hr class="main-books-list-ul-li-hr">
+        `;
+    booksList.appendChild(bookItem);
+  }
+
+  removeBook() {
+    books = books.filter((book) => book.title !== this.title);
   }
 }
 
-let books = new Books();
+function updateLocalStorage() {
+  // Add item to local storage
+  localStorage.setItem('booksArray', JSON.stringify(books));
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const book = new Book(formTitle.value, formAuthor.value);
+  book.addBookToView(formTitle.value, formAuthor.value);
+  updateLocalStorage();
+});
+
+// Remove books from the List and from the books array
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('main-books-list-ul-li-button')) {
+    e.target.parentElement.remove();
+
+    const book = new Book(e.target.parentElement.querySelector('.main-books-list-ul-li-title').innerText, e.target.parentElement.querySelector('.main-books-list-ul-li-author').innerText);
+    book.removeBook();
+    updateLocalStorage();
+  }
+});
+
+// Get books from local storage
+document.addEventListener('DOMContentLoaded', () => {
+  const books = JSON.parse(localStorage.getItem('booksArray'));
+  if (books) {
+    books.forEach((book) => {
+      const bookItem = new Book(book.title, book.author);
+      bookItem.addBookToView(book.title, book.author);
+    });
+  }
+});
